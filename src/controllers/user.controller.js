4ac84709cdbba14f4f1d32c55db9ceec396bed55
RajnishA1 <kumarrajnish.stm@gmail.com>
2024-06-16 13:56:42 +0530

    half video done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -12,17 +12,16 @@ const registerUser = asyncHandler(async (req, res) => {
     // 
 
     const { fullname, email, username, password } = req.body
-    console.log("email: ", email);
+   
 
 
     if (
-        [fullname, email, username, password].some((field) =>
-            field?.trim() === "")
+        [fullname, email, username, password].some((field) => field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required")
     }
 
-    const exitedUser = User.findOne({
+    const exitedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -30,11 +29,16 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(409, "UserName or email allready exits")
     }
 
-    const avatarLocalPath = req.files?.avatar[0].path;
-    const coverImageLocalPath = req.files?.coverImage[0].path;
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
 
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Avatar file is reuired")
+        throw new ApiError(400, "Avatar file is reuired") 
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
@@ -43,6 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
     if (!avatar){
         throw new ApiError(400,"Avatar file is required")
     }
+
 
     const user = await User.create({
         fullname,
